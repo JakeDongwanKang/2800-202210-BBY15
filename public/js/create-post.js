1af0daa82bac6fb525a414d1a13b29a693eba5f7
Linh Nguyen <knguyen142@my.bcit.ca>
2022-05-10 13:22:57 -0600

    Linh disabled an unvalued option when users select type of post; calculated the number of clicks on dropdown button to open/close dropdown menu properly

--- public/js/create-post.js
+++ public/js/create-post.js
@@ -1,6 +1,3 @@
-/**
- * Make the dropdown menu for "Type" visible when users click on "Select type".
-*/
 const dropdown = document.querySelector(".dropdown");
 const select = document.querySelector(".select");
 const caret = document.querySelector(".caret");
@@ -8,34 +5,59 @@ const menu = document.querySelector(".menu");
 const options = document.querySelectorAll(".menu li");
 const selected = document.querySelector(".selected");
 const question = document.getElementById("weather-type");
+let dropdownButtonClicks = 0;
 
-// Open the dropdown menu when user selects type of post.
+/**
+ * Open the dropdown menu if the number of clicks on dropdown menu is odd.
+ * Close the dropdown menu if that number is even (for example, when user wants to select type later).
+*/ 
 select.addEventListener('click', () => {
-    document.querySelector('.form-box.title').style.marginTop = '30%';
-    select.classList.toggle('select-clicked');
-    caret.classList.toggle('caret-rotate');
-    menu.classList.toggle('menu-open');
-    question.innerHTML = "";
+    dropdownButtonClicks += 1;
+    console.log(dropdownButtonClicks);
+    if (dropdownButtonClicks % 2 != 0) {
+        document.querySelector('.form-box.title').style.marginTop = '30%';
+        select.classList.toggle('select-clicked');
+        caret.classList.toggle('caret-rotate');
+        menu.classList.toggle('menu-open');
+        question.innerHTML = "";
+    } else {
+        closeDropdown();
+    }
+
 });
 
-// Close the dropdown menu and display the result after user selects a type of post.
+/**
+ * Close the dropdown menu and display the result after user selects a type of post.
+ * Then, sets the number of clicks on "Select type" dropdown button to 0. 
+*/
 options.forEach(option => {
     option.addEventListener('click', () => {
-        document.querySelector('.form-box.title').style.marginTop = '0%';
+        closeDropdown();
+
+        // Display the result after user selects a type
         selected.innerText = option.innerText;
-        select.classList.remove('select-clicked');
-        caret.classList.remove('caret-rotate');
-        menu.classList.remove('menu-open');
         options.forEach(option => {
             option.classList.remove('active');
         });
         option.classList.add('active');
-        
+
         // Add question asking for what kind of severe weather if user selects to create a post about weather condition.
         if (selected.innerText == "Weather conditions") {
             question.innerHTML = "<label>What kind of severe weather is it?</label>"
                 + "<input class='form-input' id='weatherType' placeholder='flood/ drought/ blizzard/...'>";
         }
+
+        // Set the number of clicks on dropdown button to 0
+        dropdownButtonClicks = 0;
     });
 });
 
+/**
+ * Close the dropdown menu for types of post.
+ */
+function closeDropdown() {
+    document.querySelector('.form-box.title').style.marginTop = '0%';
+    select.classList.remove('select-clicked');
+    caret.classList.remove('caret-rotate');
+    menu.classList.remove('menu-open');
+}
