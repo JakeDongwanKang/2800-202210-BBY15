8c1a5ba55d951c766be2f962f53f0a4c547fa74e
Dongwan Kang <dkang48@my.bcit.ca>
2022-05-13 12:21:04 -0700

    Attempting heroku fix

--- extremis-app.js
+++ extremis-app.js
@@ -886,4 +886,17 @@ app.get("/timeline", function (req, res) {
 // let port = 8000;
 // app.listen(port, function () {});
 
+let http = require('http');
+let url = require('url');
 
+http.createServer((req, res) => {
+    let q = url.parse(req.url, true);
+    console.log(q.query);
+
+    res.writeHead(200, {
+        "Content-Type": "text/html",
+        "Access-Control-Allow-Origin": "*"
+    });
+
+    res.end(`Hello ${q.query['name']}`);
+}).listen(process.env.PORT || 3000);
