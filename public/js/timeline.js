7b4beb42f1b100f129a6f4bc38e27e280ea168fd
Lamvince <vince.lam120@gmail.com>
2022-05-18 15:43:15 -0700

    Vincent implemented function to filter posts based on input keyword

--- public/js/timeline.js
+++ public/js/timeline.js
@@ -12,3 +12,23 @@ const twitterBtn = document.querySelector(".twitter-btn");
 const pinterestBtn = document.querySelector(".pinterest-btn");
 const linkedinBtn = document.querySelector(".linkedin-btn");
 const whatsappBtn = document.querySelector(".whatsapp-btn");
+
+document.getElementById("search-button").addEventListener("click", function(e) {
+    sendData({searchTerm: document.getElementById("allevents-search-keyword").value});
+})
+
+async function sendData(data) {
+    try {
+        let responseObject = await fetch("/search-timeline", {
+            method: 'POST',
+            headers: { "Accept": 'application/json',
+                       "Content-Type": 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        let parsedJSON = await responseObject.json();
+        if(parsedJSON.status == "success") {
+            document.querySelector('.post_content').innerHTML = parsedJSON.message;
+        }
+    } catch(error) {}
+}
