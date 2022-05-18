a2d1cd5260b3736773e641c32e7e6722f2c39b96
Dongwan Kang <dkang48@my.bcit.ca>
2022-05-17 17:15:35 -0700

    Jake changed some grid for mobile view on adbout-us.html as well as a little twist on the easter egg. Also the image of small totoro has been adjusted for convenience.

--- public/js/about-us.js
+++ public/js/about-us.js
@@ -1,8 +1,10 @@
 function changeImg() {
-    var image = document.getElementById('myImg');
-    if(image.src.match("/assets/team_name_drawing.png")) {
-        image.src = "/assets/team_ghibli_drawing.png";
-    } else {
-        image.src = "/assets/team_name_drawing.png";
-    }
+    document.getElementById("ghibli-event").addEventListener("click", function(e) {
+        var image = document.getElementById('myImg');
+        if(image.src.match("/assets/team_name_drawing.png")) {
+            image.src = "/assets/team_ghibli_drawing.png";
+        } else {
+            image.src = "/assets/team_name_drawing.png";
+        }
+    }); 
 }
