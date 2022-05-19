8ff7cca0fce9d63fbccf73eafec01ac082b404cd
Lamvince <vince.lam120@gmail.com>
2022-05-18 18:35:49 -0700

    Vincent made the posts wider for desktop

--- public/js/timeline.js
+++ public/js/timeline.js
@@ -7,8 +7,30 @@ toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
 
-const facebookBtn = document.querySelector(".facebook-btn");
-const twitterBtn = document.querySelector(".twitter-btn");
-const pinterestBtn = document.querySelector(".pinterest-btn");
-const linkedinBtn = document.querySelector(".linkedin-btn");
-const whatsappBtn = document.querySelector(".whatsapp-btn");
+// Original jQuery code from https://css-tricks.com/text-fade-read-more/
+// Modified by Vincent Lam
+$(".card .button").click(function() {
+    var totalHeight = 0
+  
+    var $el = $(this);
+    var $p  = $el.parent();
+    var $up = $p.parent();
+    var $ps = $up.find("div");
+    
+    $ps.each(function() {
+      totalHeight += $(this).outerHeight();
+      totalHeight += 12;
+    });
+          
+    $up
+      .css({
+        "height": $up.height(),
+        "max-height": 9999
+      })
+      .animate({
+        "height": totalHeight
+      });
+  
+    $p.fadeOut();
+    return false;
+  });
